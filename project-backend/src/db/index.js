e73dbf2b7574b57599e2258524a56e5a5d1e43d1
wcoder547 <mw5667155@gmail.com>
2024-06-29 18:03:59 +0500

    connect the DB

---
+++ project-backend/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constans.js";
+const connectDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MONGODB CONNECTD !! HOST DB:${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGO DB CONNECTION Error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
--- project-backend/src/index.js
+++ project-backend/src/index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+import dotenv from "dotenv";
+dotenv.config({ path: "./env" });
+import express from "express";
+const app = express();
+
+import connectDb from "./db/index.js";
+connectDb();
+
+// import { DB_NAME } from "./constans";
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.get("error", (err) => {
+//       console.log("Error:", err);
+//       throw err;
+
+//       app.listen(process.env.PORT, () => {
+//         console.log(`APP is listening on port :${process.env.PORT}`);
+//       });
+//     });
+//   } catch (error) {
+//     console.error("ERROR:", error);
+//   }
+// })();
