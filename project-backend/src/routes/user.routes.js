69db51531037c17d9afaeeb31f2896b46eacb389
Wcoder547 <mw5667155@.com>
2024-07-21 13:10:33 +0500

    routes done

--- project-backend/src/routes/user.routes.js
+++ project-backend/src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   userLogin,
   userLogout,
   refreshAccessToken,
+  chnageCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateuserAvatar,
+  updateuserCovwer,
+  getuserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -18,6 +25,19 @@ router.route("/login").post(userLogin);
 
 //Secured Routes
 router.route("/logout").post(verifyJwt, userLogout);
-
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, chnageCurrentPassword);
+router.route("/cuurent-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account-details").patch(verifyJwt, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateuserAvatar);
+router
+  .route("/update-coverImage")
+  .patch(verifyJwt, upload.single("coverimage"), updateuserCovwer);
+router
+  .route("/channel-profile/:username")
+  .get(verifyJwt, getuserChannelProfile);
+router.route("/watch-history").get(verifyJwt, getWatchHistory);
+
 export default router;
